748f28f5272dbaea8147cef33931246703aacb48
svetoslav0 <sssvetoslavvv@gmail.com>
2019-03-29 09:57:10 +0200

    Updated 'dist' folder, removed unnecessary components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent,
-    TestComponent
+    GameComponent
   ],
   imports: [
     BrowserModule,
